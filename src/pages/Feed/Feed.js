f12e5abda5b3a361d853b88c06d4a38991a04b27
Lucas Martins Menezes <lucasma@br.ibm.com>
2022-12-09 11:58:15 -0300

    fix pagination issue

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -273,20 +273,25 @@ class Feed extends Component {
         };
         this.setState(prevState => {
           let updatedPosts = [...prevState.posts];
+          let updatedTotalPosts = prevState.totalPosts;
           if (prevState.editPost) {
             const postIndex = prevState.posts.findIndex(
               p => p._id === prevState.editPost._id
             );
             updatedPosts[postIndex] = post;
           } else {
-            updatedPosts.pop();
+            updatedTotalPosts++;
+            if (prevState.posts.length >= 2) {
+              updatedPosts.pop();
+            }
             updatedPosts.unshift(post);
           }
           return {
             posts: updatedPosts,
             isEditing: false,
             editPost: null,
-            editLoading: false
+            editLoading: false,
+            totalPosts: updatedTotalPosts
           };
         });
       })
@@ -309,7 +314,7 @@ class Feed extends Component {
     this.setState({ postsLoading: true });
     const graphqlQuery = {
       query: `
-        mutation DeletePost($id: String!) {
+        mutation DeletePost($id: ID!) {
           deletePost(id: $id)
         }`,
         variables: {
